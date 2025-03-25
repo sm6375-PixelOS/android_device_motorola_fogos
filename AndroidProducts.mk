sm6375-PixelOS/android_device_motorola_bangkk

sm6375: adapt trees for pixelos
Signed-off-by: jennie <moenyanuwu@gmail.com>

AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_bangkk.mk
+    $(LOCAL_DIR)/aosp_bangkk.mk

sm6375-PixelOS/android_device_motorola_fogos

Signed-off-by: shoiya <barryofc11@gmail.com>

AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fogos.mk
+    $(LOCAL_DIR)/aosp_fogos.mk